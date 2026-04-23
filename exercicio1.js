aed0944f5cc4643822ca5201466a43b329b7b4ab
Karla Belo <137524224+BeloKarla7@users.noreply.github.com>
2026-04-22 21:38:34 -0400

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+let nome = "Karla"
+let idade = 22
+let maiorIdade = true; // Maior de idade: verdadeiro / Menor de idade: falso
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Maior de idade: " + (maiorIdade ? "Sim" : "Não"));
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof maiorIdade);
+
+
+
