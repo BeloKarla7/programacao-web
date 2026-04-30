da44d6ce4c383ea142e9c9659ecab1c94fefede1
Karla Belo <137524224+BeloKarla7@users.noreply.github.com>
2026-04-29 20:34:02 -0400

    Add files via upload

---
+++ ControleDeFluxo/Ex6.js
@@ -0,0 +1,15 @@
+let numero = 2;
+
+switch (numero) {
+    case 1:
+        console.log("Iniciante");
+        break;
+    case 2:
+        console.log("Intermediário");
+        break;
+    case 3:
+        console.log("Avançado");
+        break;
+    default:
+        console.log("Número inválido");
+}
